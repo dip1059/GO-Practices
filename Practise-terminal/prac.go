d532b3efc95ec0e0d6ab84d494e7cd7d746bc841
Dipankar Saha <sahadipankar1059@gmail.com>
2019-04-07 21:05:18 +0600

    updated

--- Practise-terminal/prac.go
+++ Practise-terminal/prac.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
-	//var wg sync.WaitGroup
+	var wg sync.WaitGroup
+	start := time.Now()
 	c1 := make(chan string)
 	c2 := make(chan string)
-	//wg.Add(2)
-		go PrintWord(c1)
-		msg := <- c1
-		fmt.Println(msg)
-		//wg.Done()
-	go	PrintNumber(c2)
-		msg = <- c2
-		fmt.Println(msg)
-		//wg.Done()
-	//wg.Wait()
+	wg.Add(2)
+	go PrintWord(c1)
+	msg1 := <-c1
+	fmt.Println(msg1)
+	wg.Done()
+	go PrintNumber(c2)
+	msg := <-c2
+	fmt.Println(msg)
+	wg.Done()
+	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 }
 
 func PrintWord(c1 chan string) {
 	c1 <- "This is PrintWord"
-	//close(c1)
+	close(c1)
 }
 
 func PrintNumber(c2 chan string) {
