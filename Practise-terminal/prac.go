0ae0a6ff4ad6da599d5cdf38bcaa01b281293678
Dip1059 <sahadipankar1059@gmail.com>
2019-04-07 18:27:33 +0600

    updated

--- Practise-terminal/prac.go
+++ Practise-terminal/prac.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
+	//var wg sync.WaitGroup
+	c1 := make(chan string)
+	c2 := make(chan string)
+	//wg.Add(2)
+		go PrintWord(c1)
+		msg := <- c1
+		fmt.Println(msg)
+		//wg.Done()
+	go	PrintNumber(c2)
+		msg = <- c2
+		fmt.Println(msg)
+		//wg.Done()
+	//wg.Wait()
+}
 
+func PrintWord(c1 chan string) {
+	c1 <- "This is PrintWord"
+	//close(c1)
+}
+
+func PrintNumber(c2 chan string) {
+	c2 <- "This is PrintNumber"
+	close(c2)
 }
