324879b6ce1abd1a2758b84640f3d8ee5b6a574c
Dipankar Saha <sahadipankar1059@gmail.com>
2019-04-21 06:48:26 +0600

    uploaded & updated

--- Practise-terminal/prac.go
+++ Practise-terminal/prac.go
@@ -1,15 +1,85 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
-func main() {
-	var arr = []string{"hello", "world", "what's", "up"}
+type Data struct {
+	Name string
+	Sex  string
+	Age  int
+}
 
-	var parr []string
-	parr = arr
-	arr[0] = "Hello"
-	fmt.Println(parr[0])
-	fmt.Println(arr[0])
+func main() {
+	r := gin.Default()
+
+	r.GET("/", DisplayString)
+	r.GET("/json/:name/:sex/:age", JsonData)
+
+	r.LoadHTMLGlob("html/*.html")
+	r.GET("/page", LoadPage)
+	r.GET("/page2", LoadPage2)
+	r.GET("/form", LoadForm)
+	r.POST("/welcome", Welcome)
+
+	r.Run(":2000")
+}
+
+func DisplayString(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World!")
+}
+
+func JsonData(c *gin.Context) {
+	name := c.Param("name")
+	sex := c.Param("sex")
+	age, _ := strconv.Atoi(c.Param("age"))
+	var data = []Data{
+		{
+			Name: name,
+			Sex:  sex,
+			Age:  age,
+		},
+		{
+			Name: name,
+			Sex:  sex,
+			Age:  age + 2,
+		},
+	}
+	c.JSON(http.StatusOK, &data)
+}
+
+func LoadPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "page.html", nil)
+}
+
+func LoadPage2(c *gin.Context) {
+	data := Data{}
+	data.Name = "Dipankar Saha"
+	data.Age = 25
+	c.HTML(http.StatusOK, "page2.html", data)
+}
+
+func LoadForm(c *gin.Context) {
+	c.HTML(http.StatusOK, "form.html", nil)
+}
+
+func Welcome(c *gin.Context) {
+
+	name := c.PostForm("name")
+	sex := c.PostForm("sex")
+	//type casting string2int
+	age, err := strconv.Atoi(c.PostForm("age"))
+	//
+	if err != nil {
+		c.String(http.StatusOK, "Only Integer value.")
+		return
+	}
+
+	data := Data{}
+	data.Name = name
+	data.Sex = sex
+	data.Age = age
+	c.HTML(http.StatusOK, "page2.html", data)
 }
