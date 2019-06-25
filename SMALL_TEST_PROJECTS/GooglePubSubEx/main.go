2d1f0008a30f66e45345aa4cacee7edbcef22b4d
Dip1059 <sahadipankar1059@gmail.com>
2019-06-26 01:51:34 +0600

    updated

--- SMALL_TEST_PROJECTS/GooglePubSubEx/main.go
+++ SMALL_TEST_PROJECTS/GooglePubSubEx/main.go
@@ -4,11 +4,16 @@ import (
 	"GooglePubSubEx/common"
 	"cloud.google.com/go/pubsub"
 	"context"
-	"github.com/satori/go.uuid"
+	"encoding/json"
+	//"github.com/satori/go.uuid"
 	"google.golang.org/api/option"
 	"log"
 )
 
+type Data struct {
+	UserID string	`json:"user_id"`
+	BidsAmount string	`json:"bids_amount"`
+}
 
 func main() {
 	ctx := context.Background()
@@ -17,14 +22,14 @@ func main() {
 	projectID := "bidrace"
 
 	// Creates a client.
-	client, err := pubsub.NewClient(ctx, projectID, option.WithCredentialsFile("/home/asha/key.json"))
+	client, err := pubsub.NewClient(ctx, projectID, option.WithCredentialsFile("./bidrace-fa1799950676.json"))
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
 	topic := common.CreateTopicIfNotExists(client, "BidGenerationRequest")
 
-	id := projectID+"-sub-"+uuid.NewV4().String()
+	id := "Bidstore"
 	cfg := pubsub.SubscriptionConfig{
 		Topic: topic,
 	}
@@ -37,4 +42,27 @@ func main() {
 		log.Println("Subscribed Successfully")
 	}
 
+	data, _ := json.Marshal(map[string]string{
+		"user_id":"3",
+		"bids_amount":"2000",
+	})
+	log.Println(string(data))
+	pubResult := topic.Publish(ctx, &pubsub.Message{
+		Data: data,
+	})
+	log.Println(pubResult.Get(ctx))
+	sub := client.Subscription("Bidstore")
+	err = sub.Receive(ctx, func(ctx context.Context, message *pubsub.Message) {
+		message.Ack()
+		var d Data
+		err = json.Unmarshal(message.Data, &d)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		log.Println(string(message.Data))
+		log.Println("Got message:", d.UserID, d.BidsAmount)
+	})
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
